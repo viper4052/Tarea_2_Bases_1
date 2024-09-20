9e74a8ae482684c590da86d547e50b835104ee97
viper4052 <viper4052@hotmail.com>
2024-09-19 20:13:34 -0600

    queries de filtro y listado

---
+++ querys/SPs/dbo.FiltrarBusqueda.sql
@@ -0,0 +1,36 @@
+﻿CREATE PROCEDURE [dbo].[FiltrarBusqueda]
+	@OutResulTCode INT OUTPUT
+	, @InLetters VARCHAR(128) = null
+	, @InNumbers INT = null
+
+AS
+BEGIN
+	SET NOCOUNT ON;
+	SET @OutResulTCode = 0;
+
+	-- Busca usuario por USERNAME
+	IF @InLetters IS NOT NULL
+	(
+		SELECT Nombre FROM dbo.Empleado
+		WHERE Empleado.Nombre LIKE '%' + @InLetters + '%'
+	)
+	BEGIN 
+		SET @OutResulTCode = 50007;
+		EXEC ListarEmpleados @OutresulTcode =0;
+	END 
+
+	IF @InNumbers IS NOT NULL
+	(
+		SELECT ValorDocumentoIdentidad FROM dbo.Empleado
+		WHERE Empleado.ValorDocumentoIdentidad LIKE '%' + @InNumbers + '%'
+	)
+	BEGIN 
+		SET @OutResulTCode = 50009;
+		EXEC ListarEmpleados @OutresulTcode =0;
+	END
+
+	SET NOCOUNT OFF;
+	
+
+	SET @OutResulTCode = 0;
+END;
---
+++ querys/SPs/dbo.ListarEmpleados.sql
@@ -0,0 +1,28 @@
+﻿CREATE PROCEDURE [dbo].[ListarEmpleados]
+    @OutResulTCode INT OUTPUT
+AS
+BEGIN
+    SET NOCOUNT ON;
+    BEGIN TRY
+        -- Inicializar el valor de retorno
+        SET @OutResulTCode = 0;
+		SELECT @OutResulTCode AS OutResulTCode;  -- Este codigo se agrega solo si hay problemas para obtener este  valor como parametros
+
+
+        -- Seleccionar los empleados en orden alfabético
+        --SELECT E.[Id] -- se esconde el Id (PK) a la hora de listar empleados
+          Select E.[Nombre]
+            , E.[ValorDocumentoIdentidad]
+			, E.[SaldoVacaciones]
+        FROM dbo.Empleado E 
+        ORDER BY E.Nombre ASC; -- muestra los empleados en orden ascendente
+
+    END TRY
+    BEGIN CATCH
+         -- Establecer un código de error estándar
+        SET @OutResulTCode = 50005;
+    END CATCH;
+
+    SET NOCOUNT OFF;
+END;
+GO
