639d9f3ef74b877f098dee6d3bd518ba8879d2b8
viper4052 <viper4052@hotmail.com>
2024-09-20 22:57:25 -0600

    Agregada clase Empleado

---
+++ Tarea-2-BD/Pages/Model/Empleado.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace Tarea_2_BD.Pages.Model
+{
+	public class Empleado
+	{
+		[Key]
+		public int Id { get; set; }
+
+		[DisplayName("ValorDocumentoIdentidad")]
+		[Required]
+		public int ValorDocumentoIdentidad { get; set; }
+
+		[DisplayName("Nombre")]
+		[Required]
+		public String Nombre { get; set; }
+
+		[DisplayName("FechaContratacion")]
+		[Required]
+		public DateOnly FechaContratacion { get; set; }
+
+		[DisplayName("SaldoVacaciones")]
+		[Required]
+		public decimal SaldoVacaciones { get; set; }
+
+		[DisplayName("EsActivo")]
+		[Required]
+		public bool EsActivo { get; set; }
+
+
+	}
+}
