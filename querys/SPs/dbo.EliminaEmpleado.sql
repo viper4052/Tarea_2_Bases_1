610527c8c38a283e7d255da72e4af00c46fc8ab0
viper4052 <viper4052@hotmail.com>
2024-10-04 15:56:28 -0600

    creado SP eliminar empleado

---
+++ querys/SPs/dbo.EliminaEmpleado.sql
@@ -0,0 +1,18 @@
+﻿CREATE PROCEDURE [dbo].[EliminaEmpleado]
+	/*
+		Elimina de forma logica, solo asigna el valor de la columna
+		EsActivo a 0, y no elimina de la base de datos.
+
+		No se cual es mas comodo, si con valor de documento de identidad
+		o con el nombre unico de empleado
+	*/
+	@InRefName VARCHAR(128)
+	, @InRefDocumentoIdentidad int
+AS
+	BEGIN
+		UPDATE [dbo].[Empleado] SET EsActivo = 0
+		WHERE Empleado.Nombre LIKE @InRefName
+		/* WHERE Empleado.ValorDocumentoIdentidad LIKE @InRefDocumentoIdentidad */
+	END
+
+RETURN 0
